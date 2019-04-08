9e61642d10917fe279ab4f3319b71039311ac904
Dylan Stewart <dylan.stewart@ucalgary.ca>
2019-04-08 00:51:16 -0600

    Stuff
    Additional Commenting
    Changed project JDK, but still might be broken on lab machines.

--- src/gui/AdminConsole.java
+++ src/gui/AdminConsole.java
@@ -1,11 +1,17 @@
 package gui;
 
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 import data.Account;
 import users.Doctor;
 import users.User;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.HashMap;
@@ -24,16 +30,20 @@ public class AdminConsole {
     private JPanel createUser;
     private JComboBox comboBox1;
     private JTable accountsList;
+    private JButton signOutButton;
+    private JButton signOutButton1;
     private DefaultTableModel tableModel;
     private static String[] columnNames = {
-      "Name, First","Name, Last","Role","Username","Department", "DOB"
+            "Name, First", "Name, Last", "Role", "Username", "Department", "DOB"
     };
 
     /**
      * constructor for adminConsole
+     *
      * @todo refractor all classes for consistency
      */
     public AdminConsole() {
+        $$$setupUI$$$();
         frame.setContentPane(content);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
@@ -58,19 +68,19 @@ public class AdminConsole {
                 if (!password.equals(confirmPassword)) {
                     JOptionPane.showMessageDialog(null, "Passwords do not match");
                 } else {
-                    String toDisplay = "Create account "+username+"?";
+                    String toDisplay = "Create account " + username + "?";
                     int choice = JOptionPane.showConfirmDialog(null, toDisplay);
-                        if (choice == 0) { // CONFIRM
-                            Account.createAccount(username,password,accountType);
-                            JOptionPane.showMessageDialog(null, "Account Created");
-                            formattedTextField1.setText(null);
-                            passwordField1.setText(null);
-                            passwordField2.setText(null);
-                        } else if (choice == 1) { // REJECT AND CLEAR I.E. RESTART
-                            formattedTextField1.setText(null);
-                            passwordField1.setText(null);
-                            passwordField2.setText(null);
-                        }
+                    if (choice == 0) { // CONFIRM
+                        Account.createAccount(username, password, accountType);
+                        JOptionPane.showMessageDialog(null, "Account Created");
+                        formattedTextField1.setText(null);
+                        passwordField1.setText(null);
+                        passwordField2.setText(null);
+                    } else if (choice == 1) { // REJECT AND CLEAR I.E. RESTART
+                        formattedTextField1.setText(null);
+                        passwordField1.setText(null);
+                        passwordField2.setText(null);
+                    }
                 }
             }
         });
@@ -106,15 +116,15 @@ public class AdminConsole {
                             accountType = "Patient";
                             department = "N/A";
                     }
-                     Object[] toDisplay  = {toAdd.getFirstName(), toAdd.getLastName(),
-                             accountType, toAdd.getUsername(),department, toAdd.getBirthday()};
-                     for (int i = 0; i < tableModel.getRowCount(); i++) {
-                        if (s.equals(tableModel.getValueAt(i,3))) {
+                    Object[] toDisplay = {toAdd.getFirstName(), toAdd.getLastName(),
+                            accountType, toAdd.getUsername(), department, toAdd.getBirthday()};
+                    for (int i = 0; i < tableModel.getRowCount(); i++) {
+                        if (s.equals(tableModel.getValueAt(i, 3))) {
                             dupe = true;
                             break;
-                         }
-                     }
-                     if (!dupe) tableModel.addRow(toDisplay);
+                        }
+                    }
+                    if (!dupe) tableModel.addRow(toDisplay);
                 }
             }
         });
@@ -127,7 +137,7 @@ public class AdminConsole {
                 if (e.getClickCount() == 2) {
                     int option =
                             JOptionPane.showOptionDialog(null, "Show schedule or edit?", "User Info",
-                                    JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[] {"View", "Edit", "Cancel"}, null );
+                                    JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[]{"View", "Edit", "Cancel"}, null);
                     System.out.println(option);
                     switch (option) {
                         // view schedule
@@ -151,19 +161,52 @@ public class AdminConsole {
                 }
             }
         });
+        signOutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == (signOutButton)) {
+                    signOut();
+                }
+            }
+        });
+        signOutButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == signOutButton1) {
+                    signOut();
+                }
+            }
+        });
     }
 
     public void close() {
         frame.dispose();
     }
 
+    private void signOut() {
+        try {
+            int choice = JOptionPane.showConfirmDialog(null, "Do you want to sign out?");
+            if (choice == 0) {
+                Account.writeToFile();
+                frame.dispose();
+                Login init = new Login();
+                init.init();
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Issues saving accounts, changes may have been reverted");
+        }
+    }
+
     private void createUIComponents() {
         // TODO: place custom component creation code here
-        tableModel = new DefaultTableModel(0,0){
+        tableModel = new DefaultTableModel(0, 0) {
             private static final long serialVersionUID = 1L;
+
             public boolean isCellEditable(int row, int column) {
                 return false;
-            };
+            }
+
+            ;
         };
         for (String s : columnNames) {
             tableModel.addColumn(s);
@@ -171,4 +214,91 @@ public class AdminConsole {
         accountsList = new JTable(tableModel);
         accountsList.setDragEnabled(false);
     }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        createUIComponents();
+        content = new JPanel();
+        content.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        tabbedPane1 = new JTabbedPane();
+        tabbedPane1.setEnabled(true);
+        content.add(tabbedPane1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(800, 400), null, 0, false));
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        tabbedPane1.addTab("Administration", panel1);
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new GridLayoutManager(3, 3, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.add(panel2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        createUser = new JPanel();
+        createUser.setLayout(new GridLayoutManager(4, 2, new Insets(0, 0, 0, 0), -1, -1));
+        panel2.add(createUser, new GridConstraints(1, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        formattedTextField1 = new JFormattedTextField();
+        createUser.add(formattedTextField1, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        passwordField1 = new JPasswordField();
+        createUser.add(passwordField1, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        passwordField2 = new JPasswordField();
+        createUser.add(passwordField2, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        final JLabel label1 = new JLabel();
+        label1.setText("Username: ");
+        createUser.add(label1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label2 = new JLabel();
+        label2.setText("Password: ");
+        createUser.add(label2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label3 = new JLabel();
+        label3.setText("Confirm Password: ");
+        createUser.add(label3, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label4 = new JLabel();
+        label4.setText("Account Type: ");
+        createUser.add(label4, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        comboBox1 = new JComboBox();
+        final DefaultComboBoxModel defaultComboBoxModel1 = new DefaultComboBoxModel();
+        defaultComboBoxModel1.addElement("Administrator");
+        defaultComboBoxModel1.addElement("Doctor");
+        defaultComboBoxModel1.addElement("Patient");
+        comboBox1.setModel(defaultComboBoxModel1);
+        createUser.add(comboBox1, new GridConstraints(3, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label5 = new JLabel();
+        label5.setText("Create a New Account: ");
+        panel2.add(label5, new GridConstraints(0, 0, 1, 3, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        panel2.add(spacer1, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        signOutButton = new JButton();
+        signOutButton.setText("Sign Out");
+        panel2.add(signOutButton, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        confirmButton = new JButton();
+        confirmButton.setText("Confirm");
+        panel2.add(confirmButton, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+        tabbedPane1.addTab("Accounts", panel3);
+        final JScrollPane scrollPane1 = new JScrollPane();
+        panel3.add(scrollPane1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        final JScrollPane scrollPane2 = new JScrollPane();
+        scrollPane1.setViewportView(scrollPane2);
+        accountsList.setAutoCreateRowSorter(true);
+        accountsList.setFillsViewportHeight(true);
+        scrollPane2.setViewportView(accountsList);
+        final JPanel panel4 = new JPanel();
+        panel4.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
+        panel3.add(panel4, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        signOutButton1 = new JButton();
+        signOutButton1.setText("Sign Out");
+        panel4.add(signOutButton1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        panel4.add(spacer2, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return content;
+    }
+
 }
--- src/gui/Login.java
+++ src/gui/Login.java
@@ -1,10 +1,14 @@
 package gui;
 
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 import data.Account;
 import users.Doctor;
 import users.User;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.FileInputStream;
@@ -39,52 +43,52 @@ public class Login {
             @Override
             public void mouseReleased(MouseEvent e) {
                 super.mouseReleased(e);
-                String username  = formattedTextField1.getText();
-                    String password = String.valueOf(passwordField1.getPassword());
-                    boolean debug = debugRadioButton.isSelected();
-                    if (debug) {
-                        System.out.println(username);
-                        System.out.println(password);
-                    }
-                    try {
-                        FileInputStream fileIn;
-                        ObjectInputStream in;
-                        fileIn = new FileInputStream("accounts.ser");
-                        in = new ObjectInputStream(fileIn);
-                        HashMap<String, User> dictionary = (HashMap<String, User>)in.readObject();
-                        Account.setDictionary(dictionary);
-                        in.close();
-                        fileIn.close();
-                        User acc = Account.login(username, password);
-                        if (acc != null) {
-                            switch (acc.getAccountType()) {
-                                case 1:
-                                    frame.dispose();
-                                    AdminConsole console = new AdminConsole();
-                                    break;
-                                case 2: // deal with on other iteration
-                                    DoctorConsole docView = new DoctorConsole((Doctor) acc);
-                                    frame.dispose();
-                                    break;
-                                case 3: // deal with on other iteration
-                                    ScheduleView view = new ScheduleView(acc);
-                                    frame.dispose();
-                                    break;
-                                default:
-                                    JOptionPane.showMessageDialog(null, "Invalid Username/Password");
-                            }
+                String username = formattedTextField1.getText();
+                String password = String.valueOf(passwordField1.getPassword());
+                boolean debug = debugRadioButton.isSelected();
+                if (debug) {
+                    System.out.println(username);
+                    System.out.println(password);
+                }
+                try {
+                    FileInputStream fileIn;
+                    ObjectInputStream in;
+                    fileIn = new FileInputStream("accounts.ser");
+                    in = new ObjectInputStream(fileIn);
+                    HashMap<String, User> dictionary = (HashMap<String, User>) in.readObject();
+                    Account.setDictionary(dictionary);
+                    in.close();
+                    fileIn.close();
+                    User acc = Account.login(username, password);
+                    if (acc != null) {
+                        switch (acc.getAccountType()) {
+                            case 1:
+                                frame.dispose();
+                                AdminConsole console = new AdminConsole();
+                                break;
+                            case 2: // deal with on other iteration
+                                DoctorConsole docView = new DoctorConsole((Doctor) acc);
+                                frame.dispose();
+                                break;
+                            case 3: // deal with on other iteration
+                                PatientConsole view = new PatientConsole(acc);
+                                frame.dispose();
+                                break;
+                            default:
+                                JOptionPane.showMessageDialog(null, "Invalid Username/Password");
                         }
-
-
-                    } catch (IOException ex) {
-                        JOptionPane.showMessageDialog(outer, "No accounts detected, creating new admin account,.");
-                        HashMap<String, User> dictionary = new HashMap<>();
-                        Account.setDictionary(dictionary);
-                        Account.createAccount("admin", "123", 1);
-                    } catch (Exception ex) {
-                        JOptionPane.showMessageDialog(outer, "Issues getting account.");
-                        ex.printStackTrace();
                     }
+
+
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(outer, "No accounts detected, creating new admin account,.");
+                    HashMap<String, User> dictionary = new HashMap<>();
+                    Account.setDictionary(dictionary);
+                    Account.createAccount("admin", "123", 1);
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(outer, "Issues getting account.");
+                    ex.printStackTrace();
+                }
             }
         });
         CANCELButton.addMouseListener(new MouseAdapter() {
@@ -104,9 +108,68 @@ public class Login {
         frame.setVisible(true);
     }
 
-    public static void main(String[] args ) {
+    public static void main(String[] args) {
         Login log = new Login();
         log.init();
+    }
+
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        outer = new JPanel();
+        outer.setLayout(new GridLayoutManager(3, 1, new Insets(10, 10, 20, 10), -1, -1));
+        outer.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), null));
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayoutManager(2, 2, new Insets(0, 0, 0, 0), -1, -1));
+        outer.add(panel1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        final JLabel label1 = new JLabel();
+        label1.setText("Username: ");
+        panel1.add(label1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label2 = new JLabel();
+        label2.setText("Password: ");
+        panel1.add(label2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        passwordField1 = new JPasswordField();
+        panel1.add(passwordField1, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        formattedTextField1 = new JFormattedTextField();
+        panel1.add(formattedTextField1, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
+        outer.add(panel2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        CANCELButton = new JButton();
+        CANCELButton.setText("CANCEL");
+        panel2.add(CANCELButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        CONFIRMButton = new JButton();
+        CONFIRMButton.setText("CONFIRM");
+        panel2.add(CONFIRMButton, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridLayoutManager(2, 2, new Insets(0, 0, 0, 0), -1, -1));
+        outer.add(panel3, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        debugRadioButton = new JRadioButton();
+        debugRadioButton.setText("Debug");
+        panel3.add(debugRadioButton, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        panel3.add(spacer1, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        panel3.add(spacer2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return outer;
     }
 }
 
